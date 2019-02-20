facfa4855d9d884734b86cdb5bcc8e729f8f13b9
XinoAssassin Ni <phnjs211@outlook.com>
2019-02-20 21:53:25 +0800

    finish player page's timeline component

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 import { Timeline } from "./Timeline";
+import { isNullOrUndefined } from "util";
 
-const TimelineAPI = `https://www.gcores.com/api/volumes/${id}/timelines?&auth_exclusive=dpkynzs2q0wm9o5gi1r83fcabthl4eu`
+let id = 92962;
+let TimelineAPI = `https://www.gcores.com/api/volumes/${id}/timelines?&auth_exclusive=dpkynzs2q0wm9o5gi1r83fcabthl4eu`
 
 class Player extends Component {
-    
+  state = {
+    timelines: null
+  }
+
+  componentDidMount() {
+    let proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    fetch(proxyUrl + TimelineAPI)
+      .then(res => res.json())
+      .then((res) => {
+        if (res.status === 1 && res.results.length) {
+          this.setState({
+            timelines: res.results
+          })
+        }
+      });
+  }
+
+  render() {
+    const { timelines } = this.state;
+
+    return (
+      <div>
+        {
+          timelines && timelines.map(result => <Timeline data={result} key={result.id} />)
+        }
+      </div>
+    );
+  }
 }
 
 export default Player;
